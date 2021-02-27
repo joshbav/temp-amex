ace8f84c31e0db52cb7ca742cf7ee185ccf990a3
“joshbav” <spam@nowhere.com>
2021-02-26 17:03:54 -0800

    Scripted commit 26-Feb-2021-05:03:54PM

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ echo Pushing newly built image to dockerhub
 echo
 docker push joshbav/temp-amex:latest
 echo
-echo
+echo Pushing to github
+git add *
+git config user.name “joshbav”
+git commit -m "Scripted commit $(date +%d-%b-%Y-%I:%M:%S%p)"
+git push -u origin master
